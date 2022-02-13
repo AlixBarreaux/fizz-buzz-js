1b2346bc409769d6391df47f03427dddd38b35e7
Alix Barreaux <alixbarreaux@github.com>
2022-02-13 05:46:04 +0100

    Refactor the function (now called run) triggered when the button is pressed and add return statements to all functions

--- assets/js/fizz_buzz.js
+++ assets/js/fizz_buzz.js
@@ -28,6 +28,8 @@ function initialize() {
 
   document.getElementById("small-multiple-number-input").value = small_multiple_number;
   document.getElementById("high-multiple-number-input").value = high_multiple_number;
+
+  return;
 }
 
 
@@ -41,6 +43,8 @@ function initialize_all_html_classes_elements_content() {
 
   set_text_by_html_class("small-multiple-message", small_multiple_message);
   set_text_by_html_class("high-multiple-message", high_multiple_message);
+
+  return;
 }
 
 
@@ -54,13 +58,12 @@ function set_text_by_html_class(html_class_name, new_text) {
 }
 
 
-function process_numbers() {
+function process_results() {
   //let returnValue = "";
 
   let results_grid = document.getElementById("results-grid");
   let grid_item = "      <div class=\"grid-item\">\n        <p class=\"grid-item-text\"></p>\n      </div>\n";
   let grid_item_text_content;
-  let result = "";
 
   let index = 0;
 
@@ -119,19 +122,14 @@ function process_numbers() {
     }
     console.log(counter, fizz, buzz);
   }
+  return;
 }
 
 
-// Call and use FizzBuzz
-function fizz_buzz() {
-  let output = "";
-  let value_1 = document.getElementsByClassName("small-multiple-number").value;
-  let value_2 = document.getElementsByClassName("high-multiple-number").value;
-
-  output = process_numbers();
-  console.log("Output:", output);
-
-  //document.getElementById("results").innerHTML = output;
+// Use the main script's functionality
+function run() {
+  process_results();
+  return;
 }
 
 initialize();
